472146628fe964be8042b62e0e2b598322333012
Tara <105147656+taraelhami@users.noreply.github.com>
2025-12-27 08:57:42 +0330

    Update evaluate_lstm_multi.py

--- src/evaluate_lstm_multi.py
+++ src/evaluate_lstm_multi.py
@@ -2,9 +2,10 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import os
 
+# Directory containing processed data and trained model
 processed_dir = r"E:\project AI\vehicle-tracking-deai\data\processed"
 
-# prediction
+# prediction data 
 X = np.load(os.path.join(processed_dir, "X_lstm_multi.npy"))
 y = np.load(os.path.join(processed_dir, "y_lstm_multi.npy"))
 
@@ -19,3 +20,4 @@ rmse = np.sqrt(mean_squared_error(y, pred))
 mae = mean_absolute_error(y, pred)
 
 print(f"Model Evaluation:\nRMSE: {rmse:.3f}\nMAE: {mae:.3f}")
+
