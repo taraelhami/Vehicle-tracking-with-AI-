8c7556677bdb0ab30017cb86d5f503906c716cf6
Tara <105147656+taraelhami@users.noreply.github.com>
2025-12-27 08:55:30 +0330

    Update clean_data.py

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 
+# Path to the raw input CSV containing vehicle tracking data
 input_file = r"E:\project AI\vehicle-tracking-deai\data\vehicles_initial.csv"
+
 df = pd.read_csv(input_file)
 
 #delete bad datas
@@ -14,7 +16,11 @@ df = df.sort_values(by=["vehicle_id", "frame"])
 processed_dir = r"E:\project AI\vehicle-tracking-deai\data\processed"
 os.makedirs(processed_dir, exist_ok=True)
 
+# Path for the cleaned output CSV
 output_file = os.path.join(processed_dir, "vehicles_clean.csv")
+
+# Save the cleaned DataFrame to CSV without the index
 df.to_csv(output_file, index=False)
+
 
 print(f"Clean data saved at {output_file}")
